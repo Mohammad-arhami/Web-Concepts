9b41aa007cb13f866700c3dc640aba186f8c08ab
Mohammad-Arhami <mohammad.arhami.20050902@gmail.com>
2025-11-12 15:42:18 +0330

    add dark mode to F&B End page and change some style

--- JavaScript_files/F&B-END.js
+++ JavaScript_files/F&B-END.js
@@ -1,3 +1,119 @@
+
+const themBtn = document.querySelector(".themBtn");
+const themImg = themBtn.firstElementChild;
+const span = themBtn.lastElementChild;
+const spans = document.querySelectorAll("span");
+const h1s = document.querySelectorAll("h1");
+const h2s = document.querySelectorAll("h2");
+const h3s = document.querySelectorAll("h3");
+const lis = document.querySelectorAll("li");
+const as = document.querySelectorAll("a");
+const ps = document.querySelectorAll("p");
+const bs = document.querySelectorAll("b");
+const framework_container = document.getElementsByClassName("framework-container")[0];
+
+
+themBtn.addEventListener("click" , () => {
+    if ( themImg.classList.contains("dark") ) {
+        themImg.classList.replace("dark" , "light");
+        themImg.setAttribute("src","../Images_files/light.jpg");
+        span.innerHTML = "Light Mode";
+        document.body.style.backgroundColor = "black";
+
+        h1s.forEach((h1) => {
+            h1.style.color = "white";
+        });
+
+        h2s.forEach((h2) => {
+            h2.style.color = "white";
+        });
+
+
+        ps.forEach((p) => {
+            p.style.color = "white";
+        });
+
+        spans.forEach((span) => {
+            span.style.color = "white";
+        });
+
+        lis.forEach((li) => {
+            li.style.color = "white";
+        });
+
+        as.forEach((a) => {
+            a.style.color = "white";
+        });
+
+        bs.forEach((b) => {
+            b.style.color = "white";
+        });
+
+        framework_container.style.backgroundColor = "black";
+
+    } else{
+        themImg.classList.replace("light" , "dark");
+        themImg.setAttribute("src","../Images_files/moon-icon.svg");
+        span.innerHTML = "Dark Mode";
+        document.body.style.backgroundColor = "whitesmoke";
+
+        h1s.forEach((h1) => {
+            if (!h1.classList.contains("specific")) {
+                h1.style.color = "black";
+            };
+        });
+
+        h2s.forEach((h2) => {
+            h2.style.color = "black";
+        });
+
+
+        ps.forEach((p) => {
+            if (!p.classList.contains("specific")) {
+                p.style.color = "black";
+            };
+        });
+
+        spans.forEach((span) => {
+            if (!span.classList.contains("specific")) {
+                span.style.color = "black";
+            };
+        });
+
+        lis.forEach((li) => {
+            if (!li.classList.contains("specific")) {
+                li.style.color = "black";
+            };
+        });
+
+        as.forEach((a) => {
+            a.style.color = "black";
+        });
+
+        bs.forEach((b) => {
+            b.style.color = "black";
+        });
+
+        framework_container.style.backgroundColor = "whitesmoke";
+
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const invisible_container = document.querySelectorAll(".invisible-container");
 const icons =  Array.from(invisible_container).map((container) => {
     return container.lastElementChild
@@ -15,6 +131,26 @@ invisible_container.forEach((container) => {
         e.target.lastElementChild.innerHTML = "arrow_drop_down"; 
     });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
